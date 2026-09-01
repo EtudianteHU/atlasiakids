56b129695ec6dfa255751fa494d63b821d5099ca
uyarci hilal <hilal.uyarci@gmail.com>
2026-09-01 20:39:18 +0200

    Fix contact email sending

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -31,14 +31,11 @@ router.post("/", async (req, res, next) => {
     try {
       console.log("EMAIL_PASS exists?", !!process.env.EMAIL_PASS);
 
-    const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
