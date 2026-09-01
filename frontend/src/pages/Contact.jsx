117cac629faa71f5b8b683305932e8009e5755e3
uyarci hilal <hilal.uyarci@gmail.com>
2026-09-01 19:53:44 +0200

    fix contact frontend api

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
     setIsSending(true);
 
     try {
-      const res = await fetch("/api/contact", {
+    const res = await fetch("https://atlasiakids-backend.onrender.com/api/contact",{
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
