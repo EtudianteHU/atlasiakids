d42e557b821772b49fc0da1ab6186d16fafa516e
uyarci hilal <hilal.uyarci@gmail.com>
2026-09-02 20:20:41 +0200

    Fix Gmail

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -33,16 +33,13 @@ router.post("/", async (req, res, next) => {
   console.log("EMAIL_USER:", process.env.EMAIL_USER);
   console.log("CONTACT_EMAIL:", process.env.CONTACT_EMAIL);
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("CONTACT_EMAIL:", process.env.CONTACT_EMAIL);
       const info = await transporter.sendMail({
