10d57417e9e087a99d726926b319d3ba8a06cec0
uyarci hilal <hilal.uyarci@gmail.com>
2026-09-02 18:34:02 +0200

    Fix SMTP contact email

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -33,8 +33,8 @@ router.post("/", async (req, res, next) => {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
